df8e73d77210756843126312d090705de7f30e7c
luka <lukashinjikahvili85@gmail.com>
2023-04-08 13:54:53 +0400

    notification

---
+++ src/components/Authors/FirstUser/FirstUser.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { defaultTheme } from '../../styled/styled'
+import styled from "styled-components";
+export default function FirstUser() {
+  return (
+    <>
+    
+    </>
+  )
+}
